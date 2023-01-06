1bae3affd20a22fd1ade143e47d951c1c34e2153
HeitorPreviero <heitor.previero@optimumsupply.com.br>
2023-01-06 15:01:17 -0300

    first commit

---
+++ app/src/main/java/com/heitor/conversormoedas/MainActivity.java
@@ -0,0 +1,83 @@
+package com.heitor.conversormoedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // Contém os elementos da tela em questão. Faz o carregamento uma única vez e pode ser usado a qualquer momento dentro da classe.
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Insere o layout na activity
+        setContentView(R.layout.activity_main);
+
+        // Busca os elementos da interface
+        this.mViewHolder.editReal = this.findViewById(R.id.edit_real);
+        this.mViewHolder.textDollar = this.findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = this.findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = this.findViewById(R.id.button_calculate);
+        this.mViewHolder.buttonClear = this.findViewById(R.id.button_clear);
+
+        // Limpa os valores iniciais
+        this.clearValues();
+
+        // Adiciona evento de click no elemento
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+        this.mViewHolder.buttonClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        // Verifica se o elemento clicado é o que nos interessa
+        if (view.getId() == R.id.button_calculate) {
+
+            String value = this.mViewHolder.editReal.getText().toString();
+            if ("".equals(value)) {
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+            } else {
+                // Converte o valor informado
+                Double real = Double.valueOf(value);
+
+                // Converte valores
+                this.mViewHolder.textDollar.setText(String.format("%.2f", real / 5.35));
+                this.mViewHolder.textEuro.setText(String.format("%.2f", real / 5.63));
+            }
+        }
+
+        if (view.getId() == R.id.button_clear){
+            this.mViewHolder.textDollar.setText("");
+            this.mViewHolder.textEuro.setText("");
+            this.mViewHolder.editReal.setText("");
+        }
+
+    }
+
+    /**
+     * Limpa os valores iniciais
+     */
+    private void clearValues() {
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
+
+    /**
+     * Padrão ViewHolder
+     */
+    private static class ViewHolder {
+        private EditText editReal;
+        private TextView textDollar;
+        private TextView textEuro;
+        private Button buttonCalculate;
+        private Button buttonClear;
+    }
+}
